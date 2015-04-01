974235ead0723599c5547664c381eb0f9da03ab9
Geoffrey Gilmore <ggilmore@mit.edu>
2015-04-01 12:08:25 -0400

    updated specifications for Machine.scala

--- src/main/scala/Machine.scala
+++ src/main/scala/Machine.scala
@@ -3,17 +3,48 @@
  */
 
 
-case class State(name:String)
+/**
+ * 
+ * @param descr a description for what this state represents.
+ */
+case class State(descr:String)
 
 
-case class Machine(initialState:State, finalState:State, nTransitions:Map[State, Set[State]], dtransitions: Map[State, Map[Char, Set[State]]])
+/**
+ * A class the represents a series of states that is designed to test input strings to see if they match the regex that 
+ * this class represents (and no other regex). 
+ * 
+ * @param initialState the initial "start" state of this machine. 
+ * @param finalState the final "matching" state of this matchine
+ * @param nTransitions describes transitions from one state to another state that aren't contingent on any particular 
+ *                     character
+ * @param dtransitions describes transitions from one state to another state that are contingent on a particular 
+ *                     character
+ */
+case class Machine(initialState:State, finalState:State, nTransitions:Map[State, Set[State]],
+                   dtransitions: Map[State, Map[Char, Set[State]]])
 
 object Machine {
-  def singleChar(initialState: State, finalState: State, char: Char): Machine = {
-    val dTransitions = Map(initialState -> Map(char, Set(finalState)))
-    Machine(initialState, finalState, Map(), dTransitions)
+
+  /**
+   * Generates a machine that matches a single character 'char'
+   * @param char the character that we want to match
+   * @return a machine that matches 'char' and nothing else
+   */
+  def singleChar(char: Char): Machine = {
+    val newInitial = State(s"initial state for singleChar $char.")
+    val newFinal = State(s"final state for singleChar $char.")
+    val dTransitions = Map(newInitial -> Map(char, Set(newFinal)))
+    Machine(newInitial, newFinal, Map(), dTransitions)
   }
 
+  /**
+   *
+   * @param firstMachine the first machine that is part of the "OR"
+   * @param secondMachine the second machine that is part of the "OR"
+   * @return a machine that matches iff the firstMachine matches OR the secondMachine matches,
+   *         i.e. firstMachine|secondMachine
+   */
   def or(firstMachine: Machine, secondMachine: Machine): Machine = {
     val newInitial: State = State(s"initial state for OR of $firstMachine and $secondMachine.")
     val newFinal: State = State(s"final state for OR of $firstMachine and $secondMachine.")
@@ -25,6 +56,13 @@ object Machine {
       firstMachine.dtransitions ++ secondMachine.dtransitions)
   }
 
+  /**
+   *
+   * @param firstMachine the first machine that is part of the "OR"
+   * @param secondMachine the second machine that is part of the "OR"
+   * @return a machine that matches iff both the firstMachine matches and then the secondMachine matches,
+   *         i.e. firstMachinesecondMachine
+   */
   def and(firstMachine: Machine, secondMachine: Machine): Machine = {
     val newInitial: State = State(s"initial state for AND of $firstMachine and $secondMachine.")
     val newFinal: State = State(s"final state for AND of $firstMachine and $secondMachine.")
@@ -36,6 +74,11 @@ object Machine {
       firstMachine.dtransitions ++ secondMachine.dtransitions)
   }
 
+  /**
+   *
+   * @param target the machine that we want to create the "oneOrMore" out of
+   * @return a machine that matches iff the input matches target or , targettarget, or targettargettarget, i.e. target+
+   */
   def oneOrMore(target: Machine): Machine = {
     val newInitial: State = State(s"initial state for oneOrMore of $target,")
     val newFinal: State = State(s"final state for oneOrMore of $target.")
@@ -46,6 +89,12 @@ object Machine {
     Machine(newInitial, newFinal, target.nTransitions ++ newNTrans, target.dtransitions)
   }
 
+  /**
+   *
+   * @param target the machine that we want to create the "zeroOrOne" out of
+   * @return a machine that matches iff the input matches a single target, or doesn't match a single target at all, 
+   *         i.e. target?
+   */
   def zeroOrOne(target: Machine): Machine = {
     val newInitial: State = State(s"initial state for zeroOrOne of $target.")
     val newFinal: State = State(s"final state for zeroOrOne of $target.")
@@ -56,6 +105,13 @@ object Machine {
     Machine(newInitial, newFinal, target.nTransitions ++ newNTrans, target.dtransitions)
   }
 
+
+  /**
+   *
+   * @param target the machine that we want to create the "oneOrMore" out of
+   * @return a machine that matches iff the input doesn't match target at all, or the input matches target or ,
+   *         targettarget, or targettargettarget, i.e. target*
+   */
   def someOrNone(target: Machine): Machine = {
    val tempMachine = oneOrMore(target)
     val prevTranstions:Set[State] = tempMachine.nTransitions(tempMachine.initialState)
---
+++ src/main/scala/MachineBuilder.scala
@@ -0,0 +1,7 @@
+/**
+ * Created by gmgilmore on 3/31/15.
+ */
+object MachineBuilder {
+
+
+}
